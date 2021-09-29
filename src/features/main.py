8eae9fc3546ee36f8044d629bfebf1c598d2d8fd
Diego Vieira <dgavieira@users.noreply.github.com>
2021-09-29 19:13:17 -0400

    Setting Up OAuthHandler
    Setando Handler de autorização e mascarando API e User Keys

--- src/features/main.py
+++ src/features/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from constants import *
+import tweepy as tw
+
+auth = tw.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tw.API(auth)
+
+tweet = api.update_status("Esse Sofrimento do Caralho ano que vem é o Vasco")
